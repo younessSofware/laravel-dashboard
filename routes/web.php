1ba5057358dabeb226588286db543cf839d3f4c1
younessSofware <youness3.toyouness18@gmail.com>
2022-08-20 15:29:24 +0100

    modify procfile

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', 'App\Http\Controllers\ProductController@index');
+Route::get('/', function(){
+    echo 'hello';
+});
 
 
 Auth::routes();
